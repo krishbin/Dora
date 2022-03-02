b9105b779b1794c62976d1ecbbce3e27fb1769d3
Krishbin Paudel <krishbin@outlook.com>
2022-03-03 03:36:54 +0545

    controls: increased FOV and draw distance, movement with WASD
    increased the draw distance as the houses got very large.
    added WASD movement in the world.

--- common/controls.cpp
+++ common/controls.cpp
@@ -18,14 +18,13 @@ glm::mat4 getViewMatrix(){
 glm::mat4 getProjectionMatrix(){
 	return ProjectionMatrix;
 }
-
-glm::vec3 position = glm::vec3( 0, 10, 20 );
+glm::vec3 position = glm::vec3( 0, 10, 10 );
 // horizontal angle : toward -Z
 float horizontalAngle = 3.14f;
 // vertical angle : 0, look at the horizon
 float verticalAngle = 0.0f;
 // Initial Field of View
-float initialFoV = 45.0f;
+float initialFoV = 55.0f;
 
 float speed = 20.0f; // 3 units / second
 float mouseSpeed = 0.1f;
@@ -60,26 +59,26 @@ void computeMatricesFromInputs(){
     glm::vec3 up = glm::cross( right, direction );
 
     // Move forward
-    if (glfwGetKey(window, GLFW_KEY_UP ) == GLFW_PRESS){
+    if (glfwGetKey(window, GLFW_KEY_UP ) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_W ) == GLFW_PRESS){
         position += direction * deltaTime * speed;
     }
     // Move backward
-    if (glfwGetKey(window, GLFW_KEY_DOWN ) == GLFW_PRESS){
+    if (glfwGetKey(window, GLFW_KEY_DOWN ) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_S ) == GLFW_PRESS){
         position -= direction * deltaTime * speed;
     }
     // Strafe right
-    if (glfwGetKey(window, GLFW_KEY_RIGHT ) == GLFW_PRESS){
+    if (glfwGetKey(window, GLFW_KEY_RIGHT ) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_D ) == GLFW_PRESS){
         position += right * deltaTime * speed;
     }
     // Strafe left
-    if (glfwGetKey(window, GLFW_KEY_LEFT ) == GLFW_PRESS){
+    if (glfwGetKey(window, GLFW_KEY_LEFT ) == GLFW_PRESS || glfwGetKey(window, GLFW_KEY_A ) == GLFW_PRESS){
         position -= right * deltaTime * speed;
     }
 
     float FoV = initialFoV;// - 5 * glfwGetseWheel();
 
     // Projection matrix : 45&deg; Field of View, 4:3 ratio, display range : 0.1 unit <-> 100 units
-    ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 100.0f);
+    ProjectionMatrix = glm::perspective(glm::radians(FoV), 4.0f / 3.0f, 0.1f, 500.0f);
     // Camera matrix
     ViewMatrix       = glm::lookAt(
         position,           // Camera is here
